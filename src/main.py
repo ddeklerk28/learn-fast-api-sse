269d97ddef916cdeb5f86d79d9a78f6b3ef123f2
Dirk de Klerk <ddeklerk@mintel.com>
2026-04-23 09:44:30 +0200

    Chore: added working prime stream

--- src/main.py
+++ src/main.py
@@ -1,37 +1,9 @@
-from collections.abc import AsyncIterable
-
-import asyncio
 from fastapi import FastAPI
 from fastapi.sse import EventSourceResponse
-from pydantic import BaseModel
-from starlette.responses import StreamingResponse
+
+from .utils import calculate_primes_for_generator
 
 app = FastAPI()
-
-class Item(BaseModel):
-    name: str
-    description: str | None
-
-items = [
-    Item(name="Plumbus", description="A multi-purpose household device."),
-    Item(name="Portal Gun", description="A portal opening device."),
-    Item(name="Meeseeks Box", description="A box that summons a Meeseeks."),
-]
-
-async def calculate_primes_for(target_number):
-    for num in range(0, target_number + 1):
-        if num < 2:
-            continue
-
-        is_prime = test_for_prime(num)
-        if is_prime:
-            yield f"data: {num}"
-
-def test_for_prime(num):
-    for i in range(2, num):
-        if num % i == 0:
-            return False
-    return True
 
 @app.get("/")
 async def home():
@@ -39,4 +11,5 @@ async def home():
 
 @app.get("/items/stream", response_class=EventSourceResponse)
 async def sse_items():
-    return StreamingResponse(calculate_primes_for(1000), media_type="text/event-stream")
+    async for num in calculate_primes_for_generator(1000):
+        yield num
--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,14 @@
-def calculate_primes_for(target_number):
+import asyncio
+
+async def calculate_primes_for_generator(target_number):
     for num in range(0, target_number + 1):
         if num < 2:
             continue
 
         is_prime = test_for_prime(num)
         if is_prime:
-            yield f"data: {num}"
+            await asyncio.sleep(0.2)
+            yield num
 
 def test_for_prime(num):
     for i in range(2, num):
